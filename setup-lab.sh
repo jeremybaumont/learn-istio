ca46dcea300021f57b34a56ba7782796d64158a3
Jeremy Baumont <jeremy.baumont@gmail.com>
2021-06-12 10:59:58 +1000

    fix: fix setup kubernetes and istio lab script

--- setup-lab.sh
+++ setup-lab.sh
@@ -5,7 +5,7 @@ echo "Deleting any kind clusters..."
 kind delete clusters --all
 
 echo "Setting kernel parameter... net/netfilter/nf_conntrack_max"
-sudo sysctl -w net.netfilter.nf_conntrack_max=393216
+sudo sysctl -w net.netfilter.nf_conntrack_max=${1:-393216}
 
 echo "Installing kubernetes cluster..."
 kind create cluster --name istio-lab --config kind-istio-lab.yaml 
@@ -14,7 +14,7 @@ echo "Setting cluster info context..."
 kubectl cluster-info --context kind-istio-lab
 
 echo "Checking kubernetes pods status..."
-sleep 2
+sleep 10 
 kubectl get pods -n kube-system
 
 echo "Initialising istio operator..."
@@ -24,12 +24,14 @@ echo "Installing istio operator"
 kubectl apply -f istio-operator.yaml
 
 echo "Checking istios pods status..."
-sleep 2
+sleep 15 
 kubectl get pods -n istio-system
 
 echo "Enabling istio sidecar injection on default namespace..."
 kubectl label namespace default istio-injection=enabled
 
 echo "Installing shpod kubernetes objects/pods... and enabling sidecar injection on shpod namespace..."
+kubectl apply -f shpod.yaml
 kubectl label namespace shpod istio-injection=enabled
+kubectl delete po shpod -n shpod
 kubectl apply -f shpod.yaml
